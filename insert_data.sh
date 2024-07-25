ffbea039a28bc2f0d92b3b107194c696629aa80c
Brooks Barnhouse <tsetsefly.programming@gmail.com>
2024-07-25 01:16:42 +0000

    Save progress: 1033.1

--- insert_data.sh
+++ insert_data.sh
@@ -56,6 +56,7 @@ do
   then
     # get major_id
     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+    echo $MAJOR_ID
     # if not found
     # set to null
     # insert student
